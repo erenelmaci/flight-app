434be1aced7e2937bdb937df787ee3a90e38aa38
Eren Elmacı <erenx1623@gmail.com>
2023-05-23 20:39:47 +0300

    up

---
+++ flight/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+# '/flight/':
+urlpatterns = [
+]
+
+# ---------- Router ----------
+# from rest_framework.routers import DefaultRouter
+# from .views import UserCreateView, UserView
+# router = DefaultRouter()
+# router.register('', UserView) # permissions.IsAdminUser
+# urlpatterns += router.urls
